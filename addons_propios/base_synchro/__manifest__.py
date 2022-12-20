fe72928fcced31926b8a42c0536921bd1ffb5456
Alejandro Concepción <116598782+alconor-inurban@users.noreply.github.com>
2022-12-20 11:02:05 -0500

    Agregando modulo base_synchro (alconsoft)

---
+++ addons_propios/base_synchro/__manifest__.py
@@ -0,0 +1,21 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "AlconSoft - Multi-DB Synchronization",
+    "version": "14.0-22.12.20",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Multi-DB Synchronization",
+    "author": "OpenERP SA, Serpent Consulting Services Pvt. Ltd. - Alconsoft",
+    "website": "http://www.serpentcs.com",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "images": ["static/description/Synchro.png"],
+    "depends": ["base","stock", "jobcostphasecat"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/base_synchro_view.xml",
+        "views/base_synchro_view.xml",
+        "views/res_request_view.xml",
+    ],
+    "installable": True,
+}
